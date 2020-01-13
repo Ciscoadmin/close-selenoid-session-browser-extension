925eefa9d4d33bf34e7afa70093ffea7fdf905fe
Dmitry Bolgarov <dmytro_bolgarov@epam.com>
2020-01-14 00:14:19 +0300

    Made workaround for alert issue in FF

--- Firefox/background2.js
+++ Firefox/background2.js
@@ -4,6 +4,8 @@ browser.contextMenus.onClicked.addListener(contextMenuAction);
 
 function contextMenuAction(info, tab){
 	if(info != null){
+    // var alertWindow = `alert('location:', window.location.href);` 
+    
     var correctSelenoidURL = extractSessionId(info.linkUrl);
     console.log("Force close Selenoid session using "+correctSelenoidURL);
     var req = new XMLHttpRequest;
@@ -12,7 +14,8 @@ function contextMenuAction(info, tab){
     if (req.readyState == 4) {
             if(req.status!=200){
                 console.log("Requested url: " +correctSelenoidURL+ "\n" + "HTTP Status Code: "+req.status);
-                browser.tabs.executeScript({code : `alert("Incorect HTTP Status Code was received")` });
+                // browser.tabs.executeScript({code : `alert("Requested url "+"\\n"+ "Test line 2");`});
+                // browser.tabs.executeScript({code : `var testmessage = "message"; alert("Requested url "+"\\n"+testmessage );`});
                 // console.log("Raw response data: "+ "\n" +req.responseText);
             }
         }
